4245061c6aa1847597465475df7f1c73130af76c
hartamatamatama <katonojutsu340@gmail.com>
2025-10-03 14:16:32 +0700

    menambah komentar dan melakukan testing pembuatan objek mahasiswa dan asisten dosen

--- bin/tugas_week_3_1123150117.dart
+++ bin/tugas_week_3_1123150117.dart
@@ -1,27 +1,34 @@
+// Class induk untuk merepresentasikan data mahasiswa
 class Mahasiswa {
-  // Properties (atribut)
+  // Properties (atribut) mahasiswa
   String nama;
   String nim;
   String jurusan;
   int angkatan;
-  // Constructor
+
+  // Constructor untuk membuat objek mahasiswa
   Mahasiswa({
     required this.nama,
     required this.nim,
     required this.jurusan,
     required this.angkatan,
   });
-  // Method
+
+  // Method untuk menampilkan data mahasiswa
   void tampilkanData() {
-    print("Nama : $nama");
-    print("NIM : $nim");
-    print("Jurusan : $jurusan");
+    print("Nama     : $nama");
+    print("NIM      : $nim");
+    print("Jurusan  : $jurusan");
     print("Angkatan : $angkatan");
   }
 }
 
+// Class turunan untuk merepresentasikan mahasiswa yang menjadi asisten dosen
 class AsistenDosen extends Mahasiswa {
+  // Property tambahan untuk mata kuliah yang diasisten
   String mataKuliah;
+
+  // Constructor dengan parameter tambahan mata kuliah
   AsistenDosen({
     required String nama,
     required String nim,
@@ -29,10 +36,41 @@ class AsistenDosen extends Mahasiswa {
     required int angkatan,
     required this.mataKuliah,
   }) : super(nama: nama, nim: nim, jurusan: jurusan, angkatan: angkatan);
-  // Override method
+
+  // Override method tampilkanData untuk menambahkan info mata kuliah
   @override
   void tampilkanData() {
-    super.tampilkanData();
-    print("Asisten MK: $mataKuliah");
+    super.tampilkanData(); // Tampilkan data dasar mahasiswa
+    print("Asisten MK: $mataKuliah"); // Tambahkan info mata kuliah
   }
+}
+
+// Fungsi utama program
+void main() {
+  // Membuat objek mahasiswa biasa
+  var mhs = Mahasiswa(
+    nama: "I Ketut Gunawan",
+    nim: "1123150118",
+    jurusan: "Teknik Informatika",
+    angkatan: 2025,
+  );
+
+  // Menampilkan data mahasiswa biasa
+  print("=== Data Mahasiswa ===");
+  mhs.tampilkanData();
+
+  print('\n' + '=' * 30 + '\n');
+
+  // Membuat objek asisten dosen
+  var asdos = AsistenDosen(
+    nama: "Farhan Rasisprawira Hartama",
+    nim: "1123150117",
+    jurusan: "Teknik Informatika",
+    angkatan: 2023,
+    mataKuliah: "Mobile Apps",
+  );
+
+  // Menampilkan data asisten dosen
+  print("=== Data Asisten Dosen ===");
+  asdos.tampilkanData();
 }
